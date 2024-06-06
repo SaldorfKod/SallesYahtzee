8cfceba98c86daa64f5643f9fe82c05abba65c80
Salle Larsen <salle.larsen@gmail.com>
2024-06-06 14:59:02 +0200

    final stroke of the brush

--- test_yahtzee3.py
+++ test_yahtzee3.py
@@ -1,4 +1,3 @@
-#from yahtzee3 import YahtzeeMainClass
 from yahtzee3 import rollIt
 from yahtzee3 import checkYahtzee
 from yahtzee3 import main
@@ -8,22 +7,20 @@ from die import Die
 
 def test_is_yahtzee_when_all_dice_matches():
     dice = [Die(), Die(), Die(), Die(), Die()]
-    
+    #Set all dice to value of 6
     for die in dice:
         die.value = 6
-    
-    # Assert something?
+    #Check if it now is yahtzee
     assert checkYahtzee(dice) == 6
 
 
 def test_is_not_yahtzee_when_all_dice_not_matching_each_other():
     dice = [Die(), Die(), Die(), Die(), Die()]
-    
+    #Set all dice to same value
     for die in dice:
         die.value = 6
-        
+    #Change the value of one die
     dice[0].value = 2
-    
-    # Assert something?
+    #Is it now NOT yahtzee?
     assert checkYahtzee(dice) == 0
 
--- yahtzee3.py
+++ yahtzee3.py
@@ -2,75 +2,69 @@ from die import Die
 
 #separat funktion för att rulla 5 tärningar
 #return värde 0-6: vilken du fick yahtzee i eller 0 för förlust
-def rollIt():
+def roll_it():
     #define 5 dice
     dice = [Die(), Die(), Die(), Die(), Die()]
     #set the value of each die as the result of a roll
     for i, d in enumerate(dice):
-        d.DieRoll()
+        d.die_roll()
         #print the index and value of each die
         print(f"{i}: {d}")
     #return the list of the dice
     return(dice)
     
 #Accept a list of 5 dice
-def checkYahtzee(preRolledDice):
+def check_yahtzee(pre_rolled_dice):
     #Assume we have yahtzee
-    yahFlag = True
+    yahtzee_flag = True
     #Start at index 1, compare to previous
     for j in range(1, 5):
-        if preRolledDice[j].value != preRolledDice[j-1].value:
+        if pre_rolled_dice[j].value != pre_rolled_dice[j-1].value:
             #If they are not the same it is not yahtzee
-            yahFlag = False
+            yahtzee_flag = False
     #If the were all the same, flag is still true
-    if yahFlag == True:
+    if yahtzee_flag == True:
         #Return the value of the yahtzee. Index does not matter
-        return(preRolledDice[1].value)
+        return(pre_rolled_dice[1].value)
     else:
         #A return value of 0 will indcate no yahtzee
         return(0)
 
 
 
-def main():
-    #Do we have yahtzee yes/no
-    yahValue = 0
+if __name__ == "__main__":
+    #0 for no yahtzee or the value of the yahtzee
+    yahtzee_value = 0
     #We will change this to quit the game
-    keepItGoing = True
+    keep_it_going = True
     #The turn we are on
-    intTurn = 0
+    turn = 0
     print("Welcome to Yahtzee!")
-    while keepItGoing == True:
+    while keep_it_going == True:
         #+1 to turn since we start at 0
-        print(f"Starting turn: {intTurn+1} of 3, rolling dice")
-        #To check if we have yahtzee we call the checkYahtzee function..
-        #..with the list returned by the rollIt function
-        yahValue = checkYahtzee(rollIt())
+        print(f"Starting turn: {turn+1} of 3, rolling dice")
+        #To check if we have yahtzee we call the check_yahtzee function..
+        #..with the list returned by the roll_it function
+        yahtzee_value = check_yahtzee(roll_it())
         #With returned value we can print what value we got yahtzee in
-        if yahValue > 0 and yahValue < 7:
-            print(f"You got YAHTZEE! in {yahValue}'s")
+        if yahtzee_value > 0 and yahtzee_value < 7:
+            print(f"You got YAHTZEE! in {yahtzee_value}'s")
             print(f"You won! Game over.")
             break
         #With 0 as return value we can easily print a lose message
-        if yahValue == 0:
+        if yahtzee_value == 0:
             print(f"Bad luck! :( No yahtzee this time")
-            intTurn += 1
+            turn += 1
         #You are only allowed 3 tries
-        if intTurn < 3:
+        if turn < 3:
             if input("Want to throw again? (y for yes, anything else for no) ") != "y":
                 print(f"Game over. Come again.")
                 break
-        if intTurn > 2:
+        if turn > 2:
             #User can restart the game if they like to
             if input("Game over! Want to play again? ") == "y":
-                intTurn = 0
+                turn = 0
             else:
                 #Else we will quit the game
-                keepItGoing = not keepItGoing
+                keep_it_going = not keep_it_going
                 break
-
-
-    #YahtzeeMainClass()
-
-if __name__ == '__main__':
-    main()
