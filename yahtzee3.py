b8c87f85fe46f342871086a657cbc8d4df343458
Salle Larsen <salle.larsen@gmail.com>
2024-06-11 13:00:13 +0200

    svenska. varför?

--- yahtzee3.py
+++ yahtzee3.py
@@ -1,70 +1,78 @@
 from die import Die
 
-#separat funktion för att rulla 5 tärningar
-#return värde 0-6: vilken du fick yahtzee i eller 0 för förlust
+# Funktion för att göra ett tärningskast
+# Returnera värde 0-6: vilken du fick yahtzee i, eller 0 för förlust
 def roll_it():
-    #define 5 dice
+    # Skapa 5 tärningar (objekt)
     dice = [Die(), Die(), Die(), Die(), Die()]
-    #set the value of each die as the result of a roll
+    # Kasta tärningarna och registrera värden
     for i, d in enumerate(dice):
         d.die_roll()
-        #print the index and value of each die
+        # Skriv ut index och värde för varje tärning
         print(f"{i}: {d}")
-    #return the list of the dice
+    # Returnera listan på tärningsobjekt
     return(dice)
     
-#Accept a list of 5 dice
+# Ta emot en lista på tärningsobejkt
 def check_yahtzee(pre_rolled_dice):
-    #Assume we have yahtzee
+    #Anta att vi har yahtzee
     yahtzee_flag = True
-    #Start at index 1, compare to previous
+    # Börja på plats 2 (index 1) och hämför med den innan
     for j in range(1, 5):
         if pre_rolled_dice[j].value != pre_rolled_dice[j-1].value:
-            #If they are not the same it is not yahtzee
+            # Om någon av två tärningar inte är samma värde är det ej yahtzee
             yahtzee_flag = False
-    #If the were all the same, flag is still true
+    # Om det är yahtzee ändras inte flaggan
     if yahtzee_flag == True:
-        #Return the value of the yahtzee. Index does not matter
+        # Returnera värdet på yahtzeen. Värdet är samma oavsett tärning/index
         return(pre_rolled_dice[1].value)
     else:
-        #A return value of 0 will indcate no yahtzee
+        #Om det inte är yahtzee returnera värde 0
         return(0)
 
 
-
+# Brasklapp för att inte råka köra annan kod än tänkt, om vi t.ex. 
+# vill använda en funktion från programmet någon annanstans
 if __name__ == "__main__":
     #0 for no yahtzee or the value of the yahtzee
     yahtzee_value = 0
-    #We will change this to quit the game
+    # Variabel för att avsluta loop
     keep_it_going = True
-    #The turn we are on
+    # Vilken tur vi är på
     turn = 0
+
     print("Welcome to Yahtzee!")
     while keep_it_going == True:
-        #+1 to turn since we start at 0
+        # +1 till turn så vi inte startar på turn 0
         print(f"Starting turn: {turn+1} of 3, rolling dice")
-        #To check if we have yahtzee we call the check_yahtzee function..
-        #..with the list returned by the roll_it function
+
+        #För att se om vi fick yahtzee kallar vi på check:yahtzee...
+        #... med listan vi fick som return från rollit
         yahtzee_value = check_yahtzee(roll_it())
-        #With returned value we can print what value we got yahtzee in
+
+        # Om det är yahtzee skriver vi ut det returnerade 
         if yahtzee_value > 0 and yahtzee_value < 7:
             print(f"You got YAHTZEE! in {yahtzee_value}'s")
+            # Och avslutar spelet
             print(f"You won! Game over.")
             break
-        #With 0 as return value we can easily print a lose message
+
+        # Om det inte är yahtzee skriver skriver vi annat meddelande
         if yahtzee_value == 0:
             print(f"Bad luck! :( No yahtzee this time")
             turn += 1
-        #You are only allowed 3 tries
+
+        # Du får bara tre kast
         if turn < 3:
             if input("Want to throw again? (y for yes, anything else for no) ") != "y":
                 print(f"Game over. Come again.")
                 break
+
+        # Efter tre kast får du välja om du vill köra igen
         if turn > 2:
-            #User can restart the game if they like to
             if input("Game over! Want to play again? ") == "y":
                 turn = 0
             else:
-                #Else we will quit the game
+                # Om du inte väljer "y" avslutas spelet
                 keep_it_going = not keep_it_going
                 break
